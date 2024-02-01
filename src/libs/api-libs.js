3f68290d23be41d43bb0780e361946666c36e0df
rastuak <149692923+rastuak@users.noreply.github.com>
2024-02-01 19:47:11 +0700

    changes

--- src/libs/api-libs.js
+++ src/libs/api-libs.js
@@ -3,10 +3,3 @@ export const getConvResponse = async (from,to,amount) => {
     const conv = await response.json()
     return conv
 }
-
-
-export const getConvres = async ( from, to, amount ) => {
-    const res = await fetch( `${process.env.NEXT_PUBLIC_BASE_URL}?api_key=${process.env.NEXT_PUBLIC_MY_KEY}&from=${from}&to=${to}&amount=${amount}` )
-    const data = await res.json()
-    return data
-}
